7a69c3ad780dea05dd395dbc8f5a39b453c87a6c
Oscar David Myerston Vega <oscar.myerston@dev275.local>
2022-12-12 15:10:46 -0500

    feat: handle view adjustment for keyboard

---
+++ Password/Password/Utils/UIResponser+Utils.swift
@@ -0,0 +1,28 @@
+//
+//  UIResponser+Utils.swift
+//  Password
+//
+//  Created by Oscar David Myerston Vega on 12/12/22.
+//
+
+import UIKit
+
+extension UIResponder {
+    
+    private struct Static {
+        static weak var responder: UIResponder?
+    }
+    
+    /// Finds the current first responder
+    ///  - Returns: the current UIResponder if it exists
+    static func currentFirst() -> UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
+    
+    @objc private func _trap() {
+        Static.responder = self
+    }
+}
+
